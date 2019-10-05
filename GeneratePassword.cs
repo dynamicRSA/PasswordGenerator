e31ed9220dae6458fcf4143f3ba5ad167ec1d3a7
Sven <svenprinsloo@gmail.com>
2019-10-05 20:49:14 +0200

    Initial Commit
    Currently working - some cleanup required
    TODO: Docs

---
+++ GeneratePassword.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasswordGenerator
+{
+    class GeneratePassword
+    {
+
+        /// <summary>
+        /// This will generate a password based on the first name(s), last name and bank ID
+        /// of a person.
+        /// </summary>
+        /// <param name="first">The first name(s) of the person, space seperated</param>
+        /// <param name="second">The Person's last name</param>
+        /// <param name="third">The person's bank ID</param>
+        /// <returns>The password as a string, except if an exception is thrown</returns>
+
+        public static string generatePassword(string firstNames, string lastName, string bankID)
+        {
+            if (System.String.IsNullOrWhiteSpace(firstNames))
+            {
+                throw new System.ArgumentNullException(nameof(firstNames));
+            }
+            if (System.String.IsNullOrWhiteSpace(lastName))
+            {
+                throw new System.ArgumentNullException(nameof(lastName));
+            }
+            if (System.String.IsNullOrWhiteSpace(bankID))
+            {
+                throw new System.ArgumentNullException(nameof(bankID));
+            }
+
+            try
+            {
+                // the strings here are used to diverisfy the hash generated
+                var workString = "1207." + firstNames.ToLower() + " " + lastName.ToUpper() + "." + bankID.ToLower() + ".0105";
+                var workBytes = System.Text.Encoding.UTF8.GetBytes(workString);
+                using (var sha = System.Security.Cryptography.SHA256.Create())
+                {
+                    // use only a 3rd of the hash for the password
+                    var passwordLength = (sha.HashSize / 8) / 3;
+                    var hashPart = new byte[passwordLength];
+                    System.Array.Copy(sha.ComputeHash(workBytes), 2, hashPart, 0, passwordLength);
+                    // remove the dashes
+                    return BitConverter.ToString(hashPart).Replace("-", "");
+                }
+            }
+            catch (Exception exc)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("================================================");
+            Console.WriteLine("To generate a password, insert 3 strings that will be used to generate");
+            Console.WriteLine("Insert string for first input");
+            var input1 = Console.ReadLine();
+
+            Console.WriteLine("Insert string for second input");
+            var input2 = Console.ReadLine();
+
+            Console.WriteLine("Insert string for third input");
+            var input3 = Console.ReadLine();
+
+            var response = GeneratePassword.generatePassword(input1, input2, input3);
+
+            if (response != null)
+            {
+                Console.WriteLine("The Generated Password is: " +response);
+                Console.ReadLine();
+
+            }
+            else
+            {
+                Console.WriteLine("Failed to generate password");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
